05fb417a0e6b39d5627f5687a95319723f06a1bb
Marion Beaufrere <marion.beaufrere@gmail.com>
2018-02-26 16:50:10 +0100

    Prepare for Heroku

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -2,5 +2,6 @@ class PagesController < ApplicationController
   skip_before_action :authenticate_user!, only: [:home]
 
   def home
+    @categories = ["statut","carte de séjour","état civil","langue française","aides financières","banque","logement/domiciliation","emploi","santé","mobilité","unité familiale","naturalisation"]
   end
 end
