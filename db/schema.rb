5914af2cdef6bfd93aab8b5243a1ed5339f01a84
Marion Beaufrere <marion.beaufrere@gmail.com>
2018-02-27 17:43:07 +0100

    added category to tasks

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180227164036) do
+ActiveRecord::Schema.define(version: 20180227164227) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -32,6 +32,7 @@ ActiveRecord::Schema.define(version: 20180227164036) do
     t.integer "periodicity"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "category"
   end
 
   create_table "user_subtasks", force: :cascade do |t|
