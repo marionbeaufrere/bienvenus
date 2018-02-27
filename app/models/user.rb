e48c9de0b4c83d96597b7eb582220676fc190b06
Marion Beaufrere <marion.beaufrere@gmail.com>
2018-02-27 16:16:40 +0100

    updated user model, added basic routes

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,7 +3,6 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
-  has_many :subtasks
-  has_many :tasks, through: :subtasks
+  has_many :subtasks, through: :user_subtasks
   validates :first_name, :last_name, presence: true
 end
