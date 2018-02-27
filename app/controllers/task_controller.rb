b12c3ee638c22e11edefc753b0d509890b50b5ac
Alex Deval <al3x.dvl@gmail.com>
2018-02-27 18:33:16 +0100

    modèles OK - premières méthodes-vues

--- app/controllers/task_controller.rb
+++ app/controllers/task_controller.rb
@@ -1,2 +1,12 @@
 class TaskController < ApplicationController
+
+  def index
+    @tasks = Task.all
+  end
+
+  def show
+    @task = Task.find(params[:id])
+    @subtasks = @task.subtasks
+  end
+
 end
