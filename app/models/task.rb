45c485acea47719956ef59152ee00b980562e460
Marion Beaufrere <marion.beaufrere@gmail.com>
2018-02-28 18:25:17 +0100

    push to github to delete

--- app/models/task.rb
+++ app/models/task.rb
@@ -1,17 +1,3 @@
-# == Schema Information
-#
-# Table name: tasks
-#
-#  id              :integer          not null, primary key
-#  title           :string
-#  description     :string
-#  status          :string
-#  next_occurrence :date
-#  periodicity     :integer
-#  created_at      :datetime         not null
-#  updated_at      :datetime         not null
-#
-
 class Task < ApplicationRecord
   has_many :subtasks, dependent: :destroy
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180227164227) do
+ActiveRecord::Schema.define(version: 20180228172008) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "categories", force: :cascade do |t|
+    t.string "color"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "subtasks", force: :cascade do |t|
     t.string "title"
@@ -32,7 +38,8 @@ ActiveRecord::Schema.define(version: 20180227164227) do
     t.integer "periodicity"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.string "category"
+    t.bigint "category_id"
+    t.index ["category_id"], name: "index_tasks_on_category_id"
   end
 
   create_table "user_subtasks", force: :cascade do |t|
@@ -67,6 +74,7 @@ ActiveRecord::Schema.define(version: 20180227164227) do
   end
 
   add_foreign_key "subtasks", "tasks"
+  add_foreign_key "tasks", "categories"
   add_foreign_key "user_subtasks", "subtasks"
   add_foreign_key "user_subtasks", "users"
 end
