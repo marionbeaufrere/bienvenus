da2b65850ddb8065efa75008c2f4f832b3e084fe
tom vaillant <chroniclr@hushmail.com>
2018-02-28 18:50:59 +0100

    cloudinary and filter done

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,8 +1,26 @@
 class UsersController < ApplicationController
-  before_action :update_user_state
   # skip_after_action :verify_authorized ##comment out when you want to reintroduce Pundit policies
+  def show
+    @user = current_user
+  authorize @user
+  end
+
+  def edit
+  @user = current_user
+  authorize @user
+  end
+
+  def update
+  @user = current_user
+  authorize @user
+    if current_user.update(user_params)
+      redirect_to user_path(current_user)
+    else
+      render :edit
+    end
+  end
 
   def user_params
   params.require(:user).permit(:email, :first_name, :last_name, :photo)
-end
+  end
 end
