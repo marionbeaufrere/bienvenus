88b68d66af8de39dca1b8b3b41a7c5d00bf9c8c9
Marion Beaufrere <marion.beaufrere@gmail.com>
2018-03-01 12:14:43 +0100

    created seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,11 +1,65 @@
 puts 'Cleaning database...'
+Category.destroy_all
 Task.destroy_all
 puts 'Creating tasks and associated subtasks...'
+
+################ CATEGORIES ######################################################################################
+
+cat_a = Category.create!(
+  name: "Essentials",
+  color: "#FFD35C"
+)
+cat_b = Category.create!(
+  name: "Housing",
+  color: "#FFF8201"
+)
+cat_c = Category.create!(
+  name: "Language",
+  color: "#F37252"
+)
+cat_d = Category.create!(
+  name: "Finance",
+  color: "#C72C3A"
+)
+cat_e = Category.create!(
+  name: "Education",
+  color: "#EA3E70"
+)
+cat_f = Category.create!(
+  name: "Mobility",
+  color: "#954567"
+)
+cat_g = Category.create!(
+  name: "Healthcare",
+  color: "#4BC4D5"
+)
+cat_h = Category.create!(
+  name: "Financial Aid",
+  color: "#0180B5"
+)
+
+cat_i = Category.create!(
+  name: "Employment",
+  color: "#02B5AO"
+)
+cat_j = Category.create!(
+  name: "Family Unit",
+  color: "#8A9747"
+)
+cat_k = Category.create!(
+  name: "Naturalisation",
+  color: "#C6CCCC"
+)
+cat_l = Category.create!(
+  name: "Integration",
+  color: "#CF97D7"
+)
+
 ################ ALL TASKS ######################################################################################
 
 a = Task.create!(
 title: "Obtain a residence permit",
-category: "Essentials",
+category: cat_a,
 description: "As a refugee, you are entitled to a resident card valid 10 years, automatically recreateable.
 In some prefectures you can ask for your resident card or residence permit as soon as you sign the CIR;
 however, in other prefectures you will have to wait for the Ofpra to put together and send your civil status documents.
@@ -16,7 +70,7 @@ to open a bank account, change your driver’s licence, have access to social ho
 
 b = Task.create!(
 title: "Obtain your civil status certificates",
-category: "Essentials",
+category: cat_a,
 description: "The civil status documents issued by the Ofpra are:
 birth, marriage and death certi cates and the family record book (for marriages that took place abroad and
 for minors born outside France). For a marriage or the birth of a child in France, the town hall where they were registered
@@ -27,7 +81,7 @@ enter into a civil partnership)."
 
 c = Task.create!(
 title: "Learn French",
-category: "Language",
+category: cat_a,
 description: "Learning French is essential in everyday life in order to better understand administrative procedures, and a job and
 housing, defend your rights and make your start in society easier. It is also a right that France must repect: as a host country,
 its role is to stimulate the integration of refugees, in particular by learning French. It is vital for all refugees to speak and
@@ -36,6 +90,7 @@ Level A1 (oral and written) is the level to reach for the CIR in order to get th
 (initial diploma in the French language). If you fail the DILF, the O i can, upon request,  nance a second training course so that
 you can retake the exam. After the DILF, there is another course to reach level A2 (financed by the Ofi)."
 )
+
 
 ################### TASK 1 - SUBTASKS ######################################################################
 
