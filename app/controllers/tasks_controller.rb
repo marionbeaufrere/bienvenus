656b8396fbe3672f01de426f8e9608a3254b1a5e
tom vaillant <chroniclr@hushmail.com>
2018-03-01 17:12:26 +0100

    redirects working

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -2,12 +2,13 @@ class TasksController < ApplicationController
   # before_action :update_user_state
 
 ######### DELETE THIS ONCE PUNDIT HAS BEEN IMPLEMENTED ###################
-  skip_before_action :authenticate_user!, only: [:show, :index, :completed_task]
-  skip_after_action :verify_authorized, only: [:show, :index, :completed_task]
-  skip_after_action :verify_policy_scoped, only: [:show, :index, :completed_task]
+  skip_before_action :authenticate_user!, only: [:show, :index, :completed_task, :initializer, :update]
+  skip_after_action :verify_authorized, only: [:show, :index, :completed_task, :initializer, :update]
+  skip_after_action :verify_policy_scoped, only: [:show, :index, :completed_task, :initializer, :update]
 #########################################################################
 
-  def user_initializer
+  def initializer
+    @tasks = Task.where(position: 1..2).order('position ASC, id DESC')
   end
 
   def index
@@ -19,5 +20,12 @@ class TasksController < ApplicationController
     @task = Task.find(params[:id])
     @subtasks = @task.subtasks
     @user_subtask = UserSubtask.new
+  end
+
+  def update
+    @task = Task.find(params[:id])
+    @task.status = "completed"
+    @task.save
+    redirect_to initialize_path
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,9 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: 'pages#onboarding2'
-  resources :users, only: [:edit, :update, :show, :initializer]
-  resources :tasks, only: [:index, :show]
+  resources :users, only: [:edit, :update, :show]
+  resources :tasks, only: [:index, :show, :update]
   resources :substasks, only: [:show]
   resources :user_subtasks
+  get 'initialize', to: 'tasks#initializer', as: :initialize
 end
