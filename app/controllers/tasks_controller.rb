5b265416bab5046e7faf23d75f21285acf3496a9
Thanh an Huynh <thanh1606@gmail.com>
2018-03-01 15:46:11 +0100

    nul

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,5 +1,6 @@
 class TasksController < ApplicationController
-  before_action :update_user_state
+  # before_action :update_user_state
+  skip_after_action :verify_policy_scoped, only: [:show, :index]
   def index
     # @tasks = Task.all
     @tasks = current_user.visible_tasks
