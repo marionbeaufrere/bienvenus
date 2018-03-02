989a64f707d6de378f1a2415870cc3dcea7c2008
tom vaillant <chroniclr@hushmail.com>
2018-03-02 13:18:24 +0100

    ui flow done

---
+++ app/views/tasks/initialize_checkbox.js
@@ -0,0 +1,5 @@
+// function renderTask(){
+//   let taskDone = document.getElementById('task_<%= @task_id %>');
+//   taskDone.innerHTML = '<%= j render "tasks/initializer_checkbox", task: @task %>';
+// }
+// renderTask();
