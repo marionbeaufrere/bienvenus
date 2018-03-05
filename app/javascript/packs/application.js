e7a06cef657858a1e90949a8a63046bb9336b2eb
Alex Deval <al3x.dvl@gmail.com>
2018-03-05 10:36:37 +0100

    signup OK

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -6,7 +6,7 @@ import { intializeButton } from "../components/initialize";
 intializeButton();
 console.log("supp")
 import { hideNextButton } from '../pages/signup';
-// import { hidePreviousButton } from '../pages/signup';
+import { hidePreviousButton } from '../pages/signup';
 
 //// JS for onboarding
 
