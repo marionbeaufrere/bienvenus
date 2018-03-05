0c7f68ba4f0533a1f867e1ad5a3dadf2ad8d5066
tom vaillant <chroniclr@hushmail.com>
2018-03-05 17:12:12 +0100

    sweet alert redirect

--- app/javascript/tasks/sweetalert.js
+++ app/javascript/tasks/sweetalert.js
@@ -1,24 +1,26 @@
 import swal from 'sweetalert';
 
 function sweetAlertReturn() {
-  document.querySelector('.subtask-list').addEventListener('click', (e) => {
-    const unmarkedSubtasksCount = document.querySelectorAll(".show-subtask-btn").length;
+  const subtaskList = document.querySelector('.subtask-list');
+  if (subtaskList) {
+    subtaskList.addEventListener('click', (e) => {
+      const unmarkedSubtasksCount = document.querySelectorAll(".show-subtask-btn").length;
 
-    if (e.target.classList.contains('show-subtask-btn') && unmarkedSubtasksCount == 1) {
-    // if (e.target.classList.contains('toto')) {
-      swal({
-        title: "A nice alert",
-        text: "This is a great alert, isn't it?",
-        icon: "success",
-      });
-    }
-  });
-};
+      if (e.target.classList.contains('show-subtask-btn') && unmarkedSubtasksCount == 1) {
+      // if (e.target.classList.contains('toto')) {
+        swal({
+          title: "A nice alert",
+          text: "This is a great alert, isn't it?",
+          icon: "success",
+        }).then((value) => {
+          window.location = '/tasks';
+        });
+      }
+    });
+  }
+}
 
 export { sweetAlertReturn };
 
 
 // document.getElementById('last-subtask').addEventListener('click', () => {
-// function(){
-        //     window.location.href = 'bienvenus.org/tasks';
-        // }
--- app/models/task.rb
+++ app/models/task.rb
@@ -14,11 +14,11 @@
 #  position        :integer
 
 class Task < ApplicationRecord
-  has_many :subtasks
+  has_many :subtasks, dependent: :destroy
   has_many :user_subtasks, through: :subtasks
   belongs_to :category
 
   def completed?(user)
-    user.user_subtasks.where(subtask_id: subtasks.pluck(:id)).count >=  subtasks.count
+    user.user_subtasks.where(subtask_id: subtasks.pluck(:id)).count == subtasks.count
   end
 end
