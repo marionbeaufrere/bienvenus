d7e333e231d20f25de2b17877a74e610dae56ce1
Alex Deval <al3x.dvl@gmail.com>
2018-03-08 13:20:37 +0100

    signup enlevé

--- app/javascript/pages/signup.js
+++ app/javascript/pages/signup.js
@@ -3,11 +3,6 @@
   let previousButton = document.querySelector(".signup-control-position-left")
   let firstItem = document.getElementById('first-item');
 
-  let photo = document.getElementById('user_photo')
-
-  if (signupInputs) {
-    photo.classList.add("hidden");
-  };
 
 
 
@@ -42,6 +37,11 @@ let emailInputs = document.querySelectorAll("#middle-item input");
 let phoneInput = document.getElementById("user_phone_number");
 let buttonSignup = document.getElementById("button-signup");
 
+let photo = document.getElementById('user_photo')
+
+  if (signupInputs) {
+    photo.classList.add("hidden");
+  };
 
   if (signupInputs) {
     signupInputs.forEach((input) => {
