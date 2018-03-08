717700a7268a480f895c98d4e858b41b80fcd8d0
tom vaillant <chroniclr@hushmail.com>
2018-03-08 10:24:14 +0100

    arrows done

--- app/javascript/components/subtask_arrow.js
+++ app/javascript/components/subtask_arrow.js
@@ -2,20 +2,27 @@ function taskToggleArrow() {
 
   const arrowDown = document.querySelectorAll(".subtasks-arrow-down");
   arrowDown.forEach(function (arrow) {
-    if (arrow.classList.contains("rotater")) {
-      arrow.classList.toggle("rotater");
-    }
     arrow.addEventListener("click", (event) => {
-      arrow.classList.toggle("rotator");
+      if (arrow.classList.contains("rotator")) {
+        arrow.classList.remove("rotator");
+      } else {
+        arrow.classList.add("rotator");
+      }
     });
+    // if (arrow.classList.contains("rotater")) {
+    //   arrow.classList.remove("rotater");
+    // }
+    // arrow.addEventListener("click", (event) => {
+    //   arrow.classList.toggle("rotator");
+    // });
   });
-  const arrowSide = document.querySelectorAll(".rotator");
-  arrowSide.forEach(function (arrow) {
-    arrow.addEventListener("click", (event) => {
-      arrow.classList.toggle("rotator");
-      arrow.classList.toggle("rotater");
-    });
-  });
+  // const arrowSide = document.querySelectorAll(".rotator");
+  // arrowSide.forEach(function (arrow) {
+  //   arrow.addEventListener("click", (event) => {
+  //     arrow.classList.toggle("rotator");
+  //     arrow.classList.toggle("rotater");
+  //   });
+  // });
 };
 
 export { taskToggleArrow }
